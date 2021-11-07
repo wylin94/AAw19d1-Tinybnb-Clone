dc109bb5a4104b959c15b58fbc51199386aac12d
Adam Guan <86337369+AdamHGuan@users.noreply.github.com>
2021-11-07 14:51:35 -0800

    Update db tables

---
+++ app/models/review.py
@@ -0,0 +1,22 @@
+from .db import db
+
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    spotId = db.Column(db.Integer, db.ForeignKey("spots.id"), nullable=False)
+    userid = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    review = db.Column(db.Text, nullable=False)
+
+
+    spot = db.relationship("Spot", back_populates="reviews")
+    user = db.relationship("User", back_populates="reviews")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'spotId': self.spotId,
+            'userid': self.userid,
+            'review': self.review,
+        }
