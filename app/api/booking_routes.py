d0b03851f1d7f2af8facc6abc793413a19385c2d
Huan4Ai <aihuan96@gmail.com>
2021-11-09 10:44:17 -0500

    added edit and delete route

--- app/api/booking_routes.py
+++ app/api/booking_routes.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, redirect, url_for, request
 from app.models import Booking
 from app.models.db import db
-from flask_login import current_user
-# from app.forms.booking_form import NewBookingForm
+from flask_login import current_user, login_required
+from app.forms.NewBooking_Form import NewBookingForm
+from app.forms.EditBooking_Form import EditBookingForm
 
 booking_routes = Blueprint('bookings', __name__)
 
@@ -20,19 +21,52 @@ def get_bookings():
 @booking_routes.route('/new', methods=['POST'])
 def add_booking():
   form = NewBookingForm()
+  form["csrf_token"].data = request.cookies["csrf_token"]
 
   if form.validate_on_submit():
-    data = Booking()
-    form.populate_obj(data)
-
-    db.session.add(data)
+    new_booking = Booking(
+      spotId = form.data['spotId'],
+      userId = current_user.id,
+      startDate = form.data['startDate'],
+      endDate = form.data['endDate']
+    )
+    db.session.add(new_booking)
     db.session.commit()
 
-    return data.to_dict()
-  else:
+    return new_booking.to_dict()
 
+  else:
     return form.errors
 
-# @booking_routes.route('/<int:bookingId>', methods=['PUT'])
-# def edit_booking():
-  # form =
+@booking_routes.route('/<int:id>', methods=['PATCH'])
+def edit_booking(id):
+  form = EditBookingForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+
+  booking = Booking.query.get(id)
+
+  if not booking or booking.userId != current_user.id:
+    return {"errors": ["No authorization"]}, 401
+
+  if form.validate_on_submit():
+    booking.startDate = form.data['startDate']
+    booking.endDate = form.data['endDate']
+    db.session.commit()
+    return booking.to_dict()
+
+  else:
+    return form.errors
+
+@booking_routes.route('/<int:id>', methods=['DELETE'])
+@login_required
+def delete_booking(id):
+  userId = current_user.id
+  deleted_booking = Booking.query.get(id)
+
+  if userId == deleted_booking.userId:
+    db.session.delete(deleted_booking)
+    db.session.commit()
+    return deleted_booking.to_dict()
+    
+  else:
+    return {'errors': ['No authorization.']}, 401
