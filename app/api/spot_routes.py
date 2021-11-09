fc066b50ae427ae0b1a0f2e557c7fc42a3547cf8
Adam Guan <86337369+AdamHGuan@users.noreply.github.com>
2021-11-09 11:10:07 -0800

    Update on Create Spot feature

--- app/api/spot_routes.py
+++ app/api/spot_routes.py
@@ -22,16 +22,34 @@ def searchByCity(city):
 
 @spot_routes.route('', methods=['POST'])
 def addSpots():
-    request_json = request.get_json()
+
     newSpot = Spot(
-        userId = request_json["userId"],
-        address = request_json["address"],
-        city = request_json["city"],
-        state = request_json["state"],
-        country = request_json["country"],
-        name = request_json["name"],
-        price = request_json["price"],
+        userId = request.json["userId"],
+        address = request.json["address"],
+        city = request.json["city"],
+        state = request.json["state"],
+        country = request.json["country"],
+        name = request.json["name"],
+        price = request.json["price"],
     )
+
     db.session.add(newSpot)
     db.session.commit()
-    return request.get_json()
+    return newSpot.to_dict()
+
+# @spot_routes.route('', methods=['PATCH'])
+# def editSpot():
+
+#     editedSpot = Spot.query.get(request.json['id'])
+
+#     editedSpot.address = request.json["address"],
+#     editedSpot.city = request.json["city"],
+#     editedSpot.state = request.json["state"],
+#     editedSpot.country = request.json["country"],
+#     editedSpot.lat = request.json["lat"]
+#     editedSpot.lng = request.json["lng"]
+#     editedSpot.name = request.json["name"],
+#     editedSpot.price = request.json["price"],
+    
+#     db.session.commit()
+#     return newSpot.to_dict()
