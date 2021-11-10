61e5c80775cf8662c03f4848604cf558d8f190c9
Jarum Carr <jarumlcarr@gmail.com>
2021-11-10 06:14:37 -0800

    Login done took longer than expected. Begining reviews

--- react-app/src/components/multipurpose.js
+++ react-app/src/components/multipurpose.js
@@ -15,3 +15,31 @@ export const checkIfImageExists = (url) => {
   }
   return outcome
 }
+
+export const errorHandler = (err) => {
+  const splitErr = err.split(':')
+  if (splitErr[0] === "name " && splitErr[1] === " This field is required.") {
+    return "Please provide a name."
+  }
+  if (splitErr[0] === "password " && splitErr[1] === " This field is required.") {
+    return "Please provide a password."
+  }
+  if (splitErr[0] === "password " && splitErr[1] === " Password was incorrect.") {
+    return "Password was incorrect."
+  }
+  if (splitErr[0] === "email " && splitErr[1] === " This field is required.") {
+    return "Please provide an email."
+  }
+  if (splitErr[0] === "email " && splitErr[1] === " Email address is already in use.") {
+    return "Email address is already in use."
+  }
+  if (splitErr[0] === "pass") {
+    return "Passwords do not match."
+  }
+  if (splitErr[1] === " Email provided not found.") {
+    return "Invalid email."
+  }
+  if (splitErr[1] === " No such user exists.") {
+    return "Invalid password."
+  }
+}
