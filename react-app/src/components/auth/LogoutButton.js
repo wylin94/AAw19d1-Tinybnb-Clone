d78bfb878112d1a1c53bf1dd3955f06bcd080c6f
Adam Guan <86337369+AdamHGuan@users.noreply.github.com>
2021-11-10 22:32:31 -0800

    Add Update and Delete Spot modals
    with minor updates

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { logout } from '../../store/session';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { logout } from "../../store/session";
+import { useHistory } from "react-router-dom";
 
 const LogoutButton = () => {
-  const dispatch = useDispatch()
-  const history = useHistory()
-  const onLogout = async (e) => {
-    await dispatch(logout());
-    history.push("/")
-  };
+	const dispatch = useDispatch();
+	const history = useHistory();
+	const onLogout = async (e) => {
+		// e.preventDefault();
+		history.push("/");
 
-  return <button onClick={onLogout}>Logout</button>;
+		await dispatch(logout());
+		// history.push("/");
+	};
+
+	return <button onClick={onLogout}>Logout</button>;
 };
 
 export default LogoutButton;
